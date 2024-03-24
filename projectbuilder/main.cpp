1e009afe1e16b896ad5626e8ab81efe0e0c19f3a
Zormeister <57213163+Zormeister@users.noreply.github.com>
2024-03-25 07:40:37 +1100

    remove unused function

--- projectbuilder/main.cpp
+++ projectbuilder/main.cpp
@@ -11,7 +11,7 @@
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public
  * License along with this program; if not, see <http://www.gnu.org/licenses/>.
@@ -46,10 +46,6 @@ struct BuildInfoMapEntry {
     bool hasDeps = true;
     unsigned int numDeps;
     Dependent *dependents;
-};
-
-BuildInfoMapEntry getEntryByIndex(BuildInfoMapEntry *tbl, size_t entry) {
-    return tbl[entry];
 };
 
 BuildInfoMapEntry getEntryByName(BuildInfoMapEntry *tbl, const char *entry) {
